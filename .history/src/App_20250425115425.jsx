98febb0552bf8960e750a215c253014c1293629c
fiza155 <shaikhfiza3229@gmail.com>
2025-04-27 13:03:42 +0500

    commit done

---
+++ .history/src/App_20250425115425.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/heroSection/Hero";
+// import ParawithCards from "./components/paraAndCards/ParawithCards";
+import OurServices from "./components/cards/OurServices";
+import Marquee from "./components/marquee/Marqueelogo";
+import Footer from "./components/footer/Footer";
+import Achievements from "./components/Acheivments/Acheivments";
+// import SuccessAwards from "./components/SuccessAwards/SucessAwards";
+import ImagesSection from "./components/ImagesSection/ImageSection";
+import Career from "./components/career/Career";
+import Awards from "./components/awards/Awards";
+// import Testimonial from "./components/testimonial/Testimonials";
+import ReviewsTestimonial from "./components/reviews/Reviews";
+import IndustriesSection from "./components/industries/IndustriesSection";
+import FeaturedInsights from "./components/featureInsight/FeaturedInsights";
+function App() {
+  return (
+  <RouterProvider router={routes}>
+  );
+}
+
+export default App;
---
+++ .history/src/pages/HomePage_20250425122449.jsx
@@ -0,0 +1,51 @@
+// src/pages/HomePage.jsx
+import Header from "../components/Header/Header";
+import Navbar from "../components/Navbar/Navbar";
+import Hero from "../components/heroSection/Hero";
+// import ParawithCards from "../components/paraAndCards/ParawithCards";
+import OurServices from "../components/cards/OurServices";
+import Marquee from "../components/marquee/Marqueelogo";
+import Footer from "../components/footer/Footer";
+import Achievements from "../components/Acheivments/Acheivments";
+// import SuccessAwards from "../components/SuccessAwards/SucessAwards";
+import IndustriesSection from "../components/industries/IndustriesSection";
+import ImagesSection from "../components/ImagesSection/ImageSection";
+import Career from "../components/career/Career";
+import Awards from "../components/awards/Awards";
+import MainLayout from "../layouts/MainLayout";
+// import Testimonial from "../components/testimonial/Testimonials";
+
+function HomePage() {
+  return (
+    <>
+      <MainLayout>
+        <Header />
+        <Navbar />
+        <Hero />
+        <OurServices />
+        <IndustriesSection />
+        <FeaturedInsights />
+        <Marquee />
+
+        {/* <ParawithCards /> */}
+
+        <ImagesSection />
+
+        <Awards />
+        {/* <Testimonial /> */}
+
+        <Achievements />
+        <div style={{ height: "200vh" }}>
+          <Career />
+        </div>
+        <ReviewsTestimonial />
+
+        {/* <SuccessAwards /> */}
+
+        <Footer />
+      </MainLayout>
+    </>
+  );
+}
+
+export default HomePage;
